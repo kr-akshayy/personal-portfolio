59629afc30ffef84c61ad1029e615bb4a6ba5e6f
kr-akshayy <kumar.22scse1012770@galgotiasuniversity.edu.in>
2025-05-17 00:24:11 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll to sections if needed (optional feature)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
